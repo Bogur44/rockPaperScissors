7e5ac346b3abc5675220748c2edcd2a5575fa4c6
Bogur44 <bogusky.l@gmail.com>
2023-01-05 22:38:07 +0100

    changing tactics now

--- javascript.js
+++ javascript.js
@@ -9,43 +9,83 @@ function getComputerChoice(){
     var results = outcomes[Math.floor(Math.random() * outcomes.length)];
     return(results);
 }  
-const result = document.querySelector('.result');
-const guessSubmit = document.querySelector('.submit');
-const playerSelection = getComputerChoice();
+
+let playerScore = 0;
+let computerScore = 0;
+
 const computerSelection = getComputerChoice();
+const playerSelection = 
+//const div = document.querySelector('#answers')
+
+const rockButton = document.querySelector('.rock');
+rockButton.addEventListener('click',play)
+
+const paperButton = document.querySelector('.paper');
+rockButton.addEventListener('click',play)
+
+const scissorsButton = document.querySelector('.scissors');
+rockButton.addEventListener('click',play)
+
+function play(){
+    let compMove = getComputerChoice();
+    let status = playRound(this.id,compMove);
+    //let p = document.createElement('p');
+    if (status == 'lose'){
+        computerScore += 1;
+        
+    }
+    else if(status =='win'){
+        playerScore += 1;
+        
+    }
+    
+
+}
 
 function playRound(playerSelection, computerSelection){
     
     if (computerSelection === "rock" && playerSelection === "rock"){
-        result.textContent = 'its a draw';
+        return('draw');
     }   
     else if(computerSelection === "paper" && playerSelection === "rock"){
-        result.textContent = 'you lost';
+        return('lose');
     }   
     else if(computerSelection === "scissors" && playerSelection === "rock"){
-        result.textContent = 'you won';
+        return('win'); 
     }
     else if(computerSelection === "paper" && playerSelection === "paper"){
-        return("its a draw")
+        return("draw")
     }
     else if(computerSelection === "scissors" && playerSelection === "paper"){
-        return("you lost")
+        return("lose")
     }
     else if(computerSelection === "scissors" && playerSelection === "scissors"){
-        return("its a draw")
+        return("draw")
     }
     else if(computerSelection === "rock" && playerSelection === "scissors"){
-        return("you lost")
+        return("lose")
     }
     else if(computerSelection === "rock" && playerSelection === "paper"){
-        return("you won")
+        return("win")
     }
     else if(computerSelection === "paper" && playerSelection === "scissors"){
-        return("you won")
+        return("win")
     }
     
 }
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection))
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        if (i <= 5){    
+            playRound(playerSelection, computerSelection);
+        
+        console.log(playerSelection);
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection))
+        }
+    }
+}
+
+
+
 
